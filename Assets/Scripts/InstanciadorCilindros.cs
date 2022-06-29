13f99b4bb14871a3bd09a90169ffbc50e5bf6060
Bautista <101563508+Sebakor@users.noreply.github.com>
2022-06-28 21:06:27 -0300

    NivelesTerminados

---
+++ Assets/Scripts/InstanciadorCilindros.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanciadorCilindros : MonoBehaviour
+{
+    public GameObject cilindro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(generadorCilindros());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator generadorCilindros(){
+        while (true)
+        {
+            Instantiate(cilindro);
+            yield return new WaitForSeconds(2.5f);
+        }
+    }
+}
--- Assets/Scripts/MovimientoAuto.cs
+++ Assets/Scripts/MovimientoAuto.cs
@@ -1,57 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovimientoAuto : MonoBehaviour
 {
-    public float VelMovimiento;
     Rigidbody rb;
-    bool salto = true;
+    public float VelMovimiento;
     public float JumpForce;
     public float RotacionVel;
+    bool salto = true;
+    bool seJuega = true;
+    bool Gano = true;
+    public GameObject canvasRes;
+    public GameObject cuboConfeti;
+    GameObject clon;
+    public Text txtTiempo;
+    public Text textoGanar;
+    public Image panel;
     
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         JumpForce = 15;
-        VelMovimiento = 0.2f;
-        RotacionVel = 0.2f;
+        VelMovimiento = 0.1f;
+        RotacionVel = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y <= -2){
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            transform.position = new Vector3(2.62f,4.9f,-1.47f);
-        }
 
-        if (Input.GetKeyDown(KeyCode.Space)){
-
-            if (salto){
-                salto = false;
-                rb.AddForce(Vector3.up * JumpForce,ForceMode.Impulse);
+        if (seJuega){
+            
+            if (120 - Mathf.Floor(Time.timeSinceLevelLoad) == 0){
+                Gano = false;
+                StartCoroutine(ConfetiYCosas());
             }
-        }
 
-        if (Input.GetKey(KeyCode.D)){
-            transform.Rotate(0,RotacionVel,0);
-        }
-       
-        if (Input.GetKey(KeyCode.S)){
-            transform.Translate(VelMovimiento,0,0);
-        }
-        
-        if (Input.GetKey(KeyCode.W)){
-            transform.Translate(-VelMovimiento,0,0);
-        }
+            if (transform.position.y <= -2){
+                Gano = false;
+                StartCoroutine(ConfetiYCosas());
+            }
 
-        if (Input.GetKey(KeyCode.A)){
-            transform.Rotate(0,-VelMovimiento,0);
-        }
+            if (Input.GetKeyDown(KeyCode.Space)){
+
+                if (salto){
+                    salto = false;
+                    rb.AddForce(Vector3.up * JumpForce,ForceMode.Impulse);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.D)){
+                transform.Rotate(0,RotacionVel,0);
+            }
         
+            if (Input.GetKey(KeyCode.S)){
+                transform.Translate(VelMovimiento,0,0);
+            }
+            
+            if (Input.GetKey(KeyCode.W)){
+                transform.Translate(-VelMovimiento,0,0);
+            }
+
+            if (Input.GetKey(KeyCode.A)){
+                transform.Rotate(0,-VelMovimiento,0);
+            } 
+
+            txtTiempo.text = (Mathf.Floor(Time.timeSinceLevelLoad)).ToString();
+        }
         
     }
 
@@ -66,5 +84,45 @@ public class MovimientoAuto : MonoBehaviour
             salto = true;
             JumpForce = 8;
         }
+        
+        if(other.gameObject.name == "Caja" || other.gameObject.name == "Cylinder(Clone)"){
+            Gano = false;
+            StartCoroutine(ConfetiYCosas());
+        }
+
+        if(other.gameObject.name == "Final"){
+
+            Gano = true;
+            StartCoroutine(ConfetiYCosas());
+
+        }
+    }
+
+    IEnumerator ConfetiYCosas(){
+        
+        seJuega = false;
+
+        if(Gano){ 
+            panel.color = Color.green;
+            textoGanar.text = "GANASTE!";
+        } else {
+            panel.color = Color.red;
+            textoGanar.text = "PERDISTE...";
+        }
+
+        transform.position = new Vector3(4.5f,3.2f,-1.47f);
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        
+        yield return new WaitForSeconds(2);
+
+        for (int i = 0; i < 25; i++)
+        {
+            clon = Instantiate(cuboConfeti);
+            Destroy(clon, 2);
+        }
+
+        yield return new WaitForSeconds(1);
+
+        canvasRes.SetActive(true);
     }
 }
